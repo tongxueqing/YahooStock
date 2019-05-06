6d1c1ee5692b04683791a271b6d4a6c2cf9c62c7
CarnoZhao <273806108@qq.com>
2019-05-06 14:39:34 -0400

    addTestReturn

--- NN_input.py
+++ NN_input.py
@@ -10,9 +10,14 @@ def get_data(sector = 'total', period = 5):
     Every row of Y is 6th day data of all the companies, there are 749 rows
     '''
     data = np.array(pd.read_csv('csvFiles/sp500_%s_close.csv' % sector, index_col = 0))
-    X = np.array([data[x:x + period,:].flatten('F') for x in range(0, data.shape[0] - 5)])
-    Y = np.array([data[x,:].flatten() for x in range(period, data.shape[0])])
-    return X, Y, X.shape[0], X.shape[1], Y.shape[1]
+#     X = np.array([data[x:x + period,:].flatten('F') for x in range(0, data.shape[0] - 5)])
+#     Y = np.array([data[x,:].flatten() for x in range(period, data.shape[0])])
+     X = np.array([data[x:x + period,:].flatten('F') for x in range(0, data.shape[0] - 5 - 1)])
+     X_test = np.array([data[x:x + period,:].flatten('F') for x in range(data.shape[0] - 6, data.shape[0] - 5)])
+     Y = np.array([data[x,:].flatten() for x in range(period, data.shape[0] - 1)])
+     Y_test = np.array([data[x,:].flatten() for x in range(data.shape[0] - 1, data.shape[0])])
+
+    return X, Y, X.shape[0], X.shape[1], Y.shape[1], X_test, Y_test
 
 if __name__ == '__main__':
     x, y = get_data()

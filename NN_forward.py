6aeba7cd7dad77f1f968020454c442d6f6a2b710
CarnoZhao <273806108@qq.com>
2019-05-06 20:59:38 -0400

    addCommentForawrd

--- NN_forward.py
+++ NN_forward.py
@@ -24,15 +24,20 @@ def forward(x, y, input_num, output_num, regularizer, hidden_num = 500):
     NN feed forward part
     first try:
         input        hidden       output
-        945 nodes -> 500 nodes -> 199 nodes
+        995 nodes -> 500 nodes -> 199 nodes
     '''
     w1 = get_weight([input_num, hidden_num], regularizer)
     b1 = get_bias([hidden_num])
     y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
     # hidden layer calculation
+    # X x W1 -> Z1; f(Z1) -> Y1
+    # (749 * 995) x (995 * 500) -> (749 * 500) 
 
     w2 = get_weight([hidden_num, output_num], regularizer)
     b2 = get_bias([output_num])
     y = tf.matmul(y1, w2) + b2
     # output layer calculation
+    # Y1 x W2 -> Y
+    # (749 * 500) x (500 * 198) -> (749 * 199) 
+
     return y, w1, w2

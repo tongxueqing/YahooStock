496aab5ed1686859680081c90e3a0b74f353cc3a
CarnoZhao <273806108@qq.com>
2019-05-07 13:55:14 -0400

    removeNormalize

--- NN_input.py
+++ NN_input.py
@@ -11,8 +11,8 @@ def get_data(sector = 'total', period = 5):
     '''
     data = pd.read_csv('csvFiles/sp500_%s_close.csv' % sector, index_col = 0) # get data
     cols = data.columns
-    for col in cols:
-        data[col] = (data[col] - min(data[col])) / (max(data[col]) - min(data[col]))
+    # for col in cols:
+    #     data[col] = (data[col] - min(data[col])) / (max(data[col]) - min(data[col]))
     data = np.array(data)
     filt = ~np.isnan(data).any(axis = 0)
     data = data[:,filt] # remove NA
@@ -22,8 +22,8 @@ def get_data(sector = 'total', period = 5):
     X = np.array([data[x:x + period,:].flatten() for x in range(0, data.shape[0] - 5)]) # 5 day as features, total 749
     Y = np.array([data[x,:].flatten() for x in range(period, data.shape[0])]) # for each 5 day, use the 6th day as the label (result)
     data = pd.read_csv('csvFilesTest/%s_test.csv' % sector, index_col = 0) # get data
-    for col in cols:
-        data[col] = (data[col] - min(data[col])) / (max(data[col]) - min(data[col]))
+    # for col in cols:
+    #     data[col] = (data[col] - min(data[col])) / (max(data[col]) - min(data[col]))
     data = np.array(data[cols])
     X_test = np.array([data[x:x + period,:].flatten() for x in range(0, data.shape[0] - 5)])
     Y_test = np.array([data[x,:].flatten() for x in range(period, data.shape[0])])
